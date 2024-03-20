90ec145997dd65d1cbe2e1e77f349b1bd21dc7c0
Vinicius Santos <87083914+vinicsans@users.noreply.github.com>
2024-03-20 10:33:37 -0300

    a

---
+++ js/header-component.js
@@ -0,0 +1,48 @@
+function applyShadowDOM() {
+    const template = document.createElement('template');
+    template.innerHTML = `
+      <style>
+        #top {
+          display: flex;
+          height: 8rem;
+          width: 100%;
+          flex-direction: row;
+          align-items: center;
+        }
+  
+        #nav_buttons {
+          display: flex;
+          margin-left: auto;
+          margin-right: 0;
+          align-items: center;
+        }
+  
+        #nav_buttons a {
+          margin: 0.5rem;
+        }
+  
+        .links a img {
+          width: 4rem;
+        }
+      </style>
+  
+      <div id="top">
+        <img src="../../assets/Logo.jpg" />
+  
+        <div id="nav_buttons">
+          <a href="./index.html">Home</a>
+          |
+          <a href="./contato.html">Contato</a>
+          |
+          <a href="./sobre.html">Sobre</a>
+        </div>
+      </div>
+    `;
+
+    const shadowRoot = document.createElement('div').attachShadow({ mode: 'open' });
+
+    shadowRoot.appendChild(template.content.cloneNode(true));
+    document.body.insertBefore(shadowRoot, document.querySelector('h2'));
+}
+
+applyShadowDOM()
